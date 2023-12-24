ba0fc75514dd2cd384e30a2de325a95574ef5e1a
Sreenivasulu <novemberautomationbatch@gmail.com>
2023-12-24 11:15:37 +0530

    Added for loop

--- flow_controls/for_while_loop.py
+++ flow_controls/for_while_loop.py
@@ -1,67 +1,93 @@
 '''This file created to practice iterative statements
 Author - Sreeni added date:20/Dec/2023 '''
 
-ls = [4,5,6,'Sreeni', True]
-t = (44,55,77,'Raghav', False)
-d = {1:"Sreeni", 2:"Raghav", 3 :"Mahesh"}
-s= {7,1,2,3,4,5,5,6,6}
-fs = frozenset({1,4,5,6,7,7,7,8})
-r = range(1,10)
-#df, series, arrays
+# ls = [4,5,6,'Sreeni', True]
+# t = (44,55,77,'Raghav', False)
+# d = {1:"Sreeni", 2:"Raghav", 3 :"Mahesh"}
+# s= {7,1,2,3,4,5,5,6,6}
+# fs = frozenset({1,4,5,6,7,7,7,8})
+# r = range(1,10)
+# #df, series, arrays
+#
+# #Syntax for
+#
+# # for value in iterativedata:
+# #     statement1
+# #     statement2
+#
+# print("The list ls is ",ls)
+# print("first value ",ls[0])
+# print("second value ",ls[1])
+# print("third value ",ls[2])
+# print("fourth ",ls[3])
+# print("fifth ",ls[4])
+# print("*"*40)
+#
+# for i in ls:
+#     ind = ls.index(i)
+#     print(f"The value of {i} present in index {ind}")
+#
+# print("*"*50)
+# for value in t:
+#     ind = t.index(value)
+#     print(f"In Tuple {t}, The value of {value} present in index {ind}")
+#
+# table  = ['profile', "email", "phone", "transaction"]
+#
+# for tn in table:
+#     col = 'id'
+#     print(f"select id, count(1) from {tn} group by {col} having count(1)>1")
+#
+# print("The keys present in d",d.keys())
+# print("The values present in d",d.values())
+# print("The items present in d ",d.items())
+#
+# for key, value in d.items():
+#     print(f"Dictionary {d}, The key is {key} and value is {value}")
+#
+# print("The set ",s)
+#
+#
+#
+# for value in s:
+#     print(f"In set {s}, The value of {value} ")
+#
+#
+# for i in range(1,11):
+#     print(i)
+#
+# print("*"*40)
+# for i in range(1,11):
+#     if i % 2 == 1 :
+#         print(i)
 
-#Syntax for
+# for i in range(1,5):
+#     print("*" * 10)
+#     print("The value of i is ", i)
+#     for j in range(1,6):
+#         print("The value of j is ", j)
+#         for k in range(1,10):
+#             print("The value of k is ", k )
+#
+# for i in range(1,21):
+#     for j in range(1,11):
+#         print(f"{i}*{j}",i * j)
 
-# for value in iterativedata:
-#     statement1
-#     statement2
+# print("#"*40)
+# for i in range(1,10):
+#     print(i)
+#     if i == 5:
+#         print("The program will end soon because of i became 5")
+#         break
+#         print("After break")
 
-print("The list ls is ",ls)
-print("first value ",ls[0])
-print("second value ",ls[1])
-print("third value ",ls[2])
-print("fourth ",ls[3])
-print("fifth ",ls[4])
-print("*"*40)
+# for i in range(1,10):
+#     if i == 8:
+#         continue
+#     print(i)
 
-for i in ls:
-    ind = ls.index(i)
-    print(f"The value of {i} present in index {ind}")
+for i in range(1,10):
+    pass
 
-print("*"*50)
-for value in t:
-    ind = t.index(value)
-    print(f"In Tuple {t}, The value of {value} present in index {ind}")
-
-table  = ['profile', "email", "phone", "transaction"]
-
-for tn in table:
-    col = 'id'
-    print(f"select id, count(1) from {tn} group by {col} having count(1)>1")
-
-print("The keys present in d",d.keys())
-print("The values present in d",d.values())
-print("The items present in d ",d.items())
-
-for key, value in d.items():
-    print(f"Dictionary {d}, The key is {key} and value is {value}")
-
-print("The set ",s)
-
-
-
-for value in s:
-    print(f"In set {s}, The value of {value} ")
-
-
-for i in range(1,11):
-    print(i)
-
-print("*"*40)
-for i in range(1,11):
-    if i % 2 == 1 :
-        print(i)
-
-for i in range(1,11):
-    for j in range(1,11):
-        print(f"{i}*{j} = ", i*j)
-
+def sum(a,b):
+    pass
--- random_fol/test.py
+++ random_fol/test.py
@@ -1,5 +1,21 @@
-str = "My salaryis 7000"
-print(str.isalnum())
+import pandas as pd
 
-str2 = "Mysalaryis"
-print(str2.isalpha())
+source = pd.read_csv(r'/Users/harish/Downloads/archive (7)/portfolio.csv')
+target = pd.read_csv(r'/Users/harish/Downloads/archive (7)/portfolio.csv')
+
+print(source.shape[0])
+print(source.shape[0])
+
+def count_val(sourcedf, targetdf):
+    source_cnt = source.shape[0]
+    target_cnt =  target.shape[0]
+    if source_cnt == target_cnt:
+        print("Count is matching")
+    else:
+        print("Count is not matching and difference is", abs(source_cnt-target_cnt))
+
+count_val(source, target)
+
+Function is set of lines code which will do specific activity
+and it gradually reduce redudancy of code
+
